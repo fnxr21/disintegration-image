be9017989bbd9e4a45a134108290595b1f9e0115
fandi.nxr <fandinur214@gmail.com>
2024-06-05 17:09:28 +0700

    update

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/disintegration/imaging"
+)
+
 func main() {
+	src, err := imaging.Open("original/1.jpg")
+
+	if err != nil {
+		log.Printf("failed to open :%v", err)
+	}
+
+	// Resize the cropped image to width = 200px preserving the aspect ratio.
+	src = imaging.Resize(src, 800, 0, imaging.Lanczos)
+
+	err = imaging.Save(src, "3L/1.jpg")
+	if err != nil {
+		log.Fatalf("failed to save image: %v", err)
+	}
 
 }
