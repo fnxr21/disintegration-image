a5d0e60d1fa1ba5c804f058438577ce37656a1eb
fandi.nxr <fandinur214@gmail.com>
2024-06-06 11:13:55 +0700

    update

--- main.go
+++ main.go
@@ -1,24 +1,68 @@
 package main
 
 import (
-	"log"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"os"
+
+	// "os"
+	"time"
 
 	"github.com/disintegration/imaging"
 )
 
+type ImageUser struct {
+	ImageKTP []byte `json:"image_ktp" `
+}
+
 func main() {
-	src, err := imaging.Open("original/1.jpg")
+	timestamp := time.Now().Add(time.Second).Unix()
+	src, err := imaging.Open("original/6.jpg", imaging.AutoOrientation(true))
 
 	if err != nil {
-		log.Printf("failed to open :%v", err)
+		// Handle errors while opening the image
+		fmt.Printf("failed to open image: %v\n", err)
+		return
 	}
 
 	// Resize the cropped image to width = 200px preserving the aspect ratio.
 	src = imaging.Resize(src, 800, 0, imaging.Lanczos)
 
-	err = imaging.Save(src, "3L/1.jpg")
+	// Create a filename with timestamp and .jpg extension
+	filename := fmt.Sprintf("3L/rnd-"+"%d.jpg", timestamp)
+	err = imaging.Save(src, filename)
+
 	if err != nil {
-		log.Fatalf("failed to save image: %v", err)
+		// Handle errors while saving the image
+		fmt.Printf("failed to save image: %v\n", err)
+		return
 	}
+
+	files, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	
+	defer files.Close()
+	
+	data, err := io.ReadAll(files)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	catchmodels := ImageUser{
+		ImageKTP: data,
+		}
+
+	cam1 := base64.StdEncoding.EncodeToString(catchmodels.ImageKTP)
+
+	fmt.Println(cam1)
+	defer os.Remove(filename)
+
+	//save to database
+	// Create and save image
 
 }
