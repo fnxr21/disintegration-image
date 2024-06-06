dcdcb1b45b797e09f6949bcaceef736662a6c009
fandi.nxr <fandinur214@gmail.com>
2024-06-06 14:48:05 +0700

    update

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module disintegration
 
 go 1.21.5
 
-require (
-	github.com/disintegration/imaging v1.6.2
-	github.com/labstack/echo/v4 v4.12.0
-)
+require github.com/labstack/echo/v4 v4.12.0
 
 require (
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -14,7 +11,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,63 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"fmt"
-	"image/jpeg"
-	"image/png"
 	"net/http"
-
-	// "io"
-
-	"github.com/disintegration/imaging"
+	
 	"github.com/labstack/echo/v4"
 )
 
-// type ImageUser struct {
-// 	ImageKTP []byte `json:"image_ktp" `
-// }
-
 func main() {
-
 	e := echo.New()
-	e.GET("/", (
-		func(c echo.Context) error {
-
-			imageUser := c.Get("dataFile")
-			Image := imageUser.([]byte)
-
-			Imageuser := base64.StdEncoding.EncodeToString(Image)
-			return c.JSON(http.StatusOK, Imageuser)
-		}))
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func UploadFile(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		files, err := c.FormFile("image")
-
-		f, _ := files.Open()
-
-		defer f.Close()
-		imgpng, _ := png.Decode(f)
-		fmt.Println(imgpng)
-		// defer imgpng.Close()
-		img, _ := jpeg.Decode(f)
-
-		srcs := imaging.Resize(img, 800, 0, imaging.Lanczos)
-
-		// defer src.Close()
-		buf := new(bytes.Buffer)
-		err = jpeg.Encode(buf, srcs, nil)
-		if err != nil {
-			fmt.Println("Error reading file:", err)
-			return err
-		}
-
-		filesa := buf.Bytes()
-
-		c.Set("dataFile", filesa)
-		return next(c)
-	}
 }
